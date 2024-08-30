e2f10df7e703bcff3c80345d0860d82508f12ce5
EvenMoreH <hubert.lewan@gmail.com>
2024-08-30 11:14:32 +0200

    created a function toString() that takes in ouptut string, free text with user specified number of variables and displayes result as a string in console using type writer effect. No there will be no need to hardcode list of all variables for each function

--- src/printing.c
+++ src/printing.c
@@ -7,6 +7,7 @@
 #include <math.h>       // Enables using math functions
 #include <windows.h>    // Unlocks windows functionalities
 #include <ctype.h>      // allows for tolower() funtion
+#include <stdarg.h>     // to operate on unknown number of arguments
 
 
 #include "../include/printing.h"
@@ -16,21 +17,42 @@
 
 int a = 14;
 int b = 20;
+int c = 40;
+int d = 50;
 
 char textBuffer[256];
 
+char textOutput[256];
 
-char testLocation[] = "Village";
+char testLocation1[] = "Village";
+char testLocation2[] = "City";
+
 
 void storyText(char textToWrite[]);
+
+void toString(char *input, const char *format, ...);
 
 int main() {            // Main function
 
     sprintf(textBuffer, "Your current HP: [%i/%i].", a, b);
     storyText(textBuffer);
 
-    sprintf(textBuffer, "You reached your destination: [%s].", testLocation);
+    sprintf(textBuffer, "You reached your destination: [%s].", testLocation1);
     storyText(textBuffer);
+
+    printf("\n\n");
+
+    toString(textOutput, "Player health is %i/%i. Player mana is %i/%i. You reached: %s. Your next stop is: %s.", a, b, c, d, testLocation1, testLocation2);
+
+    a = 2;
+    c = 21;
+
+    strcpy(textBuffer, testLocation2);
+    strcpy(testLocation2, testLocation1);
+    strcpy(testLocation1, textBuffer);
+
+
+    toString(textOutput, "Player health is %i/%i. Player mana is %i/%i. You reached: %s. Your next stop is: %s.", a, b, c, d, testLocation1, testLocation2);
 
 
     return 0;               // Ends main function
@@ -47,4 +69,18 @@ void storyText(char textToWrite[]) {
     }
     printf("\n");
     Sleep(350);
+}
+
+// 'destination' is a string that holds final output
+    // 'prompt' is a formatted text with arguments to be printed (same format as with printf)
+    // For example: "Your current HP: [%d/%d].", a, b
+void toString(char *destination, const char *prompt, ...) {
+    va_list args;
+    va_start(args, prompt);
+
+    vsprintf(destination, prompt, args);
+
+    va_end(args);
+
+    storyText(textOutput);
 }
